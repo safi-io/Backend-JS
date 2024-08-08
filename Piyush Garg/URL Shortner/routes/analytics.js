4ddc1f3d67b02ddeaad144f9e628c4afaba38285
safi-io <m.safi.ullah@outlook.com>
2024-08-08 09:18:27 +0500

    Starting work on URL Shortner once again

--- Piyush Garg/URL Shortner/routes/analytics.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const {handleAnalytics} = require('../controllers/url')
-
-const router = express.Router();
-
-router.get("/:slug", handleAnalytics);
-
-module.exports = router;
